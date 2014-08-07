9393d9e5bf058c6d366f35dd7551dfb4fb6bcb86
pmcote <pmcote1@gmail.com>
2014-08-07 08:41:47 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'); 		
+var app = express(); 
+var bodyParser = require('body-parser');
+var post_it = require('./post_it.js')
+
+app.use(bodyParser());
+
+var port = process.env.PORT || 3000;
+
+var router = express.Router();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+router.post('/', post_it.post_it); //endpoint for Celery webhook
+
+
+//hit at 
+
+app.use('/', router);
+
+app.listen(process.env.PORT || port);
+console.log('Go find yo stuff on port', port);
